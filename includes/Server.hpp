f6dc3d037844a7b381a628c8018554d30d60372c
Elbaaaaaa <bachirbella08@gmail.com>
2026-05-10 20:54:33 +0200

    added the basics of the Server class. Next to do : Server.cpp

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -13,12 +13,58 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <vector>
+#include <map>
+#include <poll.h>
+#include <string>
+
+#include "Client.hpp"
+#include "Channel.hpp"
+#include "IrcReply.hpp"
+#include "IrcMessage.hpp"
+
+
+
 class Server
 {
     private:
+    
         int _port;
+        int _socket;
+
+        bool _running;
+
+        std::string _password;
+        
+        std::vector<pollfd> _fds;
+        
+        std::map<int, Client*> _clients;
+        std::map<std::string, Channel*> _channels;
+        std::map<std::string, void (Server::*)(int, IrcMessage&)> _commands;
+
+        void initSocket();
+        void acceptNewClient();
+        void removeClient(int fd);
+        void writeToClient(int fd);
+        void handleCommand(int fd, IrcMessage& message);
+        void sendToClient(int fd, std::string message);
+
     public:
+
         Server(int port, std::string password);
         ~Server();
 
-}
+        void run();
+        
+        Client* getClientByFd(int fd);
+        Channel* getChannel(const std::string& name);
+        Client* getClientByNick(const std::string& nick);
+
+        void disconnectClient(int fd);
+        void createChannel(const std::string& name, Client& op);
+        void sendToClient(int fd, const std::string& message);
+        void removeClientFromAllChannels(Client& client);
+        void removeChannel(const std::string& name);
+};
+
+#endif
